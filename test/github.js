8cd5d51fea71cc3357cca42e34513ac0fc621b72
Andrei Rusu <andrei@andrei-mbp.local>
2016-02-16 17:33:36 +0100

    Added demo tests and configuration

---
+++ test/github.js
@@ -0,0 +1,15 @@
+module.exports = {
+  tags: ['git'],
+  'Demo test GitHub' : function (client) {
+    client
+      .url('https://github.com/nightwatchjs/nightwatch')
+      .waitForElementVisible('body', 1000)
+      .assert.title('nightwatchjs/nightwatch · GitHub')
+      .assert.visible('.container h1 strong a')
+      .assert.containsText('.container h1 strong a', 'nightwatch', 'Checking project title is set to nightwatch');
+  },
+
+  after : function(client) {
+    client.end();
+  }
+};
