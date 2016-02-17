3ca0ab92c171c76ab2e301f3f02b9e7a2b9039a0
Andrei Rusu <andrei.rusu@beatfactor.net>
2016-02-17 12:30:31 +0100

    updated tests and config

--- test/github.js
+++ test/github.js
@@ -4,7 +4,9 @@ module.exports = {
     client
       .url('https://github.com/nightwatchjs/nightwatch')
       .waitForElementVisible('body', 1000)
-      .assert.title('nightwatchjs/nightwatch · GitHub')
+      .getTitle(function(result) {
+        client.assert.equal(result.indexOf('GitHub - nightwatchjs/nightwatch'), 0, 'Page title starts with nightwatch')
+      })
       .assert.visible('.container h1 strong a')
       .assert.containsText('.container h1 strong a', 'nightwatch', 'Checking project title is set to nightwatch');
   },
--- test/nightwatch.js
+++ test/nightwatch.js
@@ -1,18 +1,9 @@
 module.exports = {
-  disabled : true,
   'Demo test NightwatchJS.org' : function (client) {
     client
       .url('http://nightwatchjs.org')
-      .waitForElementVisible('body', 1000)
-      .elements('css selector', '#index-container ul.features li', function (result) {
-
-        for (var i = 0; i < result.value.length; i++) {
-          var element = result.value[i];
-
-          var selector = '#index-container ul.features li:nth-child(' + (element.ELEMENT ) +') em';
-          client.verify.cssClassPresent(selector, 'glyphicon');
-        }
-      })
+      .waitForElementVisible('body')
+      .assert.elementsCount('#index-container ul.features li', 7)
       .end();
   }
 };
